c6cabe314403d9ca0a743805a2e5ca8cc8755e26
luca <lucalm2004@gmail.com>
2023-10-31 15:51:12 +0100

    sql

---
+++ sql/ddbb.sql
@@ -0,0 +1,28 @@
+-- Crear la tabla de Usuarios
+CREATE TABLE Usuarios (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    nombre_real VARCHAR(255) NOT NULL,
+    contraseña VARCHAR(255) NOT NULL
+);
+
+-- Crear la tabla de Amistades
+CREATE TABLE Amistades (
+    friendship_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id_1 INT NOT NULL,
+    user_id_2 INT NOT NULL,
+    estado_solicitud ENUM('pendiente', 'aceptada', 'rechazada') NOT NULL,
+    FOREIGN KEY (user_id_1) REFERENCES Usuarios(user_id),
+    FOREIGN KEY (user_id_2) REFERENCES Usuarios(user_id)
+);
+
+-- Crear la tabla de Mensajes
+CREATE TABLE Mensajes (
+    message_id INT AUTO_INCREMENT PRIMARY KEY,
+    sender_id INT NOT NULL,
+    receiver_id INT NOT NULL,
+    contenido VARCHAR(250) NOT NULL,
+    fecha_envio DATETIME NOT NULL,
+    FOREIGN KEY (sender_id) REFERENCES Usuarios(user_id),
+    FOREIGN KEY (receiver_id) REFERENCES Usuarios(user_id)
+);
