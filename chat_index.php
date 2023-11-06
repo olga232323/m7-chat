aa6044c064c8ca067b0be478862ed1a7254e317c
olga232323 <olgaclementemolina@gmail.com>
2023-11-06 15:50:40 +0100

    prueba

---
+++ chat_index.php
@@ -0,0 +1,101 @@
+<?php
+session_start();
+if (!isset($_SESSION['loginOk'])) {
+  header('Location: ' . './inc/cerrar_sesion.php');
+  exit();
+} else {
+  ?>
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./src/logopro-removebg-preview.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+      crossorigin="anonymous"></script>
+      <script src="./js/funcion_eviar.js"></script>
+    <title>Whatsapp 2</title>
+    <link rel="stylesheet" type="text/css" href="./css/chat_style.css">
+  </head>
+
+  <body>
+    <section>
+      <div class="container py-5">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card" id="chat3" style="border-radius: 15px;">
+              <div class="card-body">
+                <div class="row">
+                  <!-- Barra de búsqueda y amistades -->
+                  <div class="col-md-6 col-lg-5 col-xl-4 mb-4 mb-md-0">
+
+                    <div class="p-3">
+                      <!-- Barra de búsqueda -->
+                      <!-- Busqueda de usuario va a validar en busqueda_usuarios.php-->
+                      <form action="busqueda_usuarios.php" method="POST">
+                        <div class="input-group form-control rounded mb-3">
+                          <input type="search" class="form-control rounded" name="busqueda_realizada"
+                            id="busqueda_realizada" placeholder="Busqueda" aria-label="Busqueda"
+                            aria-describedby="search-addon" />
+                          <span class="input-group-text border-0" id="search-addon">
+                            <input class="form-control rounded" type="submit" name="buscar" value="Buscar">
+                          </span>
+                        </div>
+                      </form>
+                      <!-- Amistades -->
+                      <div data-mdb-perfect-scrollbar="true" style="position: relative; height: 400px">
+                        <ul class="list-unstyled mb-0">
+                          <!-- Buscar el chat con el usuario seleccionado -->
+                          <?php
+                          if (!isset($_SESSION['busqueda_realizada'])) {
+
+                          }
+                          // Recibimos el listado de amigos de listado_amigos.php
+                          include_once("./inc/listado_amigos.php");
+                          ?>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                  <!-- Chat general -->
+                  <div class="col-md-6 col-lg-7 col-xl-8">
+                    <!-- Listado de mensajes -->
+                    <div class="pt-3 pe-3" data-mdb-perfect-scrollbar="true" style="position: relative; height: 400px">
+                      <?php
+                      // Recibimos el listado de mensajes de listado_mensajes.php
+                      if (!isset($_SESSION["leerMensajes"])) {
+                        ?>
+                        <div class='d-flex flex-row justify-content-center align-items-center '>
+                          <div>
+                            <p class='small p-2 ms-3 mb-1 rounded-3' style='background-color: #f5f6f7;'>Selecciona una persona para chatear!</p>
+                          </div>
+                        </div>
+                        <?php
+                      } else {
+                        include_once("./inc/listado_mensajes.php");
+                      }
+                      ?>
+                    </div>
+                    <!-- Barra enviar mensaje -->
+                    <div class="text-muted d-flex justify-content-start align-items-center pe-3 pt-3 mt-2">
+                      <input type="text" class="form-control form-control-lg" id="exampleFormControlInput2"
+                        placeholder="Escriba un mensaje">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+
+  </html>
+  <?php
+}
+?>
---
+++ inc/register.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = $_POST["user"];
+    $nombreCompleto = $_POST["nombreCompleto"];
+    $password = $_POST["password"];
+
+    // Validación de datos (puedes agregar más validaciones según tus necesidades)
+    if (empty($user)) {
+        header("Location: ../indexRegister.php?emptyUsr");
+        exit;
+    } else if(empty($nombreCompleto)){
+        header("Location: ../indexRegister.php?emptyNom");
+        exit;
+    }else if(empty($password)){
+        header("Location: ../indexRegister.php?emptyPwd");
+        exit;
+    }
+
+    // Conexión a la base de datos
+    require_once("conexion.php");
+
+    // Verificar si el nombre de usuario ya está en uso
+    $query = "SELECT user_id FROM Usuarios WHERE username = ?";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "s", $user);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_store_result($stmt);
+
+    if (mysqli_stmt_num_rows($stmt) > 0) {
+        header("Location: ../indexRegister.php?usrex");
+        exit;
+    }
+
+    // Hashear la contraseña antes de almacenarla en la base de datos con BCRYPT
+    $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+    // Insertar el nuevo usuario en la base de datos
+    $query = "INSERT INTO Usuarios (username, nombre_real, contraseña) VALUES (?, ?, ?)";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "sss", $user, $nombreCompleto, $hashedPassword);
+
+    if (mysqli_stmt_execute($stmt)) {
+        // Registro exitoso, redirige al usuario a la página de inicio de sesión
+        header("Location: ../indexRegister.php?message");
+        exit;
+    } else {
+        header("Location: ../indexRegister.php?error");
+        exit;
+    }
+
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+}
+?>
