7d77c4b0498857acac83bd25d85bdfbac6203c0e
luca <lucalm2004@gmail.com>
2023-11-13 16:00:26 +0100

    versionInsertCorrecta

--- solicitudes_amistad.php
+++ solicitudes_amistad.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Solicitudes de Amistad</title>
 </head>
+
 <body>
     <h1>Solicitudes de Amistad</h1>
-    
+
     <?php
     session_start();
     include("./inc/conexion.php");
@@ -13,22 +15,22 @@
     $user_id = $_SESSION['user_id'];
 
     // Consulta para obtener las solicitudes de amistad pendientes
-    $sql = "SELECT friendship_id, username, estado_solicitud
-            FROM amistades
-            INNER JOIN usuarios ON user_id_1 = user_id
-            WHERE user_id_2 = ? AND estado_solicitud = 'pendiente'";
+    $sql = "SELECT a.friendship_id, a.estado_solicitud, u.username AS username_user_id_2, u.user_id AS user_id_user_id_2 FROM amistades a INNER JOIN usuarios u ON a.user_id_2 = u.user_id WHERE a.user_id_1 = ? AND a.estado_solicitud = 'pendiente'";
     $stmt = mysqli_stmt_init($conn);
 
     if (mysqli_stmt_prepare($stmt, $sql)) {
         mysqli_stmt_bind_param($stmt, "i", $user_id);
         mysqli_stmt_execute($stmt);
-        mysqli_stmt_bind_result($stmt, $friendship_id, $amigo_username, $estado_solicitud);
+        $result = mysqli_stmt_get_result($stmt);
 
         echo "<ul>";
-        while (mysqli_stmt_fetch($stmt)) {
+        while ($fila = mysqli_fetch_assoc($result)) {
+            $amigo_username = $fila['username_user_id_2'];
+            $friendship_id = $fila['friendship_id'];
+            $amigo_id = $fila['user_id_user_id_2'];
             echo "<li>
                     $amigo_username
-                    <a href='./inc/aceptar_solicitud.php?friendship_id=$friendship_id&amigo_username=$amigo_username'>Aceptar</a>
+                    <a href='./inc/aceptar_solicitud.php?friendship_id=$friendship_id&amigo_id=$amigo_id'>Aceptar</a>
                 </li>";
         }
         echo "</ul>";
@@ -39,4 +41,5 @@
     mysqli_close($conn);
     ?>
 </body>
+
 </html>
