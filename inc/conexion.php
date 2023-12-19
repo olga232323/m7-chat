a0352e5ef304576ea595571d1408714eefcc293e
luca <lucalm2004@gmail.com>
2023-12-19 15:20:40 +0100

    conexion.php

--- inc/conexion.php
+++ inc/conexion.php
@@ -1,15 +1,12 @@
 <?php
-$dbserver = "localhost";
-$dbuser = "root";
-$dbpwd = "";
-$dbbasedatos = "db_chat";
-
+$servidor = "mysql:dbname=db_chat;host:localhost";
+$user="root";
+$pass="";
 
 try {
-   $conn = @mysqli_connect($dbserver, $dbuser, $dbpwd, $dbbasedatos);
+    $pdo = new PDO($servidor,$user,$pass,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
-} catch (Exception $e) {
-   echo ("Error: " . $e->getMessage());
-   die();
-
+} catch (Exception $e){
+    echo "Error en la conexión con la base de datos: " . $e->getMessage();
+    die();
 }
