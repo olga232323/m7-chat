fc67f50b351f9891e093306a9b01a0c01a6cb630
luca <lucalm2004@gmail.com>
2023-12-19 15:21:33 +0100

    conexion.php

--- inc/conexion.php
+++ inc/conexion.php
@@ -4,7 +4,7 @@ $user="root";
 $pass="";
 
 try {
-    $pdo = new PDO($servidor,$user,$pass,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $conn = new PDO($servidor,$user,$pass,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
 } catch (Exception $e){
     echo "Error en la conexión con la base de datos: " . $e->getMessage();
