0bc90e3a531548cf6378d0b24bdc489eb29012c1
olga232323 <olgaclementemolina@gmail.com>
2024-01-08 16:48:19 +0100

    fixed usuarios amistades

--- inc/busqueda_usuarios.php
+++ inc/busqueda_usuarios.php
@@ -39,28 +39,30 @@ function buscarUsuarios($conn, $nombre)
 function sonAmigos($conn, $user_id_1, $user_id_2)
 {
     $consulta = "SELECT estado_solicitud FROM Amistades
-                 WHERE (:user_id_1 AND :user_id_2) OR (:user_id_1 AND :user_id_2)";
+                 WHERE (user_id_1 = :user_id_1 AND user_id_2 = :user_id_2) OR (user_id_1 = :user_id_2 AND user_id_2 = :user_id_1)";
     $stmt = $conn->prepare($consulta);
     $stmt->bindParam(':user_id_1', $user_id_1);
     $stmt->bindParam(':user_id_2', $user_id_2);
-    $stmt->bindParam(':user_id_1', $user_id_2);
-    $stmt->bindParam(':user_id_2', $user_id_1);
     $stmt->execute();
     $estado_solicitud = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    foreach($estado_solicitud as $estado_soli){
-        $soli =  $estado_soli['estado_solicitud'];
-
-    }
-
     $stmt->closeCursor();
 
+    $soli = ''; // Initialize $soli
+
+    if (!empty($estado_solicitud)) {
+        foreach ($estado_solicitud as $estado_soli) {
+            $soli =  $estado_soli['estado_solicitud'];
+        }
+    }
 
     if ($soli === 'aceptada') {
         return true; // Son amigos
     } else if ($soli === 'pendiente') {
-        return true; // No son amigos pero hay una solicitud pendiente
+        return true; // No son amigos, pero hay una solicitud pendiente
     } else {
         return false; // No son amigos
     }
 }
+
+
 ?>
